9782c946c5b11b77758beb956327d1996a82fbdf
Mye <ysfbey.13@gmail.com>
2023-03-16 18:37:55 +0300

    first commit

---
+++ day1/example.py
@@ -0,0 +1,11 @@
+dersTamamlandimi = False
+if dersTamamlandimi == True:
+    print("tik işareti")
+else:
+    print("yuvarlak boş kalsın")    
+
+girisYapmismi = True
+if girisYapmismi == True:
+    print("kurslarım menüsünü göster")
+else:
+    print("kurslarım menüsünü gizle")
